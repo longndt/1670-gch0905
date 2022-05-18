63709c790d4395ca9ef811c906cfce5c43b96dcc
longndt <longndt226@gmail.com>
2022-05-18 16:08:50 +0700

    web1

--- web1/web1/Controllers/HomeController.cs
+++ web1/web1/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace web1.Controllers
             return View();
         }
 
+        public IActionResult Greenwich()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
