dce7deb8e92f0164aa19cc9c67436af11f699971
longndt <longndt226@gmail.com>
2022-05-18 17:21:10 +0700

    web1

---
+++ web1/web1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web1.Models;
+
+namespace web1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            Student s1 = new Student(1, "Tuan", 20, 7);
+            Student s2 = new Student(2, "Minh", 19, 8);
+            Student s3 = new Student(3, "Phuong", 18, 10);
+            List<Student> students = new List<Student>()
+            {
+                s1, s2, s3
+            };
+            ViewBag.Students = students;
+            return View();
+        }
+    }
+}
--- web1/web1/Models/Student.cs
+++ web1/web1/Models/Student.cs
@@ -2,5 +2,17 @@
 {
     public class Student
     {
+        public int id { get; set; } 
+        public string name { get; set; }
+        public int age { get; set; }
+        public double grade { get; set; }
+
+        public Student (int i, string n, int a, float g)
+        {
+            this.id = i;
+            this.name = n;
+            this.age = a;
+            this.grade = g;
+        }
     }
 }
