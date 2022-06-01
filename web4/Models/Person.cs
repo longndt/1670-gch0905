cb9436c06d90283fac14a7fc0314b1ecac99a0f4
longndt <longndt226@gmail.com>
2022-06-01 15:36:45 +0700

    web

---
+++ web4/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace web4.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ web4/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace web4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public Student (int id, string name, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+    }
+}
