d9046c87a8bbeeb37d7357ef523de6cd14fb77e4
longndt <longndt226@gmail.com>
2022-06-01 13:58:19 +0700

    1670

---
+++ web3/Controllers/LaptopsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using web3.Data;
+using web3.Models;
+
+namespace web3.Controllers
+{
+    public class LaptopsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LaptopsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Laptops
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Laptop.ToListAsync());
+        }
+
+        // GET: Laptops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var laptop = await _context.Laptop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (laptop == null)
+            {
+                return NotFound();
+            }
+
+            return View(laptop);
+        }
+
+        // GET: Laptops/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Laptops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Color,Brand")] Laptop laptop)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(laptop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(laptop);
+        }
+
+        // GET: Laptops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var laptop = await _context.Laptop.FindAsync(id);
+            if (laptop == null)
+            {
+                return NotFound();
+            }
+            return View(laptop);
+        }
+
+        // POST: Laptops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Color,Brand")] Laptop laptop)
+        {
+            if (id != laptop.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(laptop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LaptopExists(laptop.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(laptop);
+        }
+
+        // GET: Laptops/Delete/5
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var laptop = await _context.Laptop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (laptop == null)
+            {
+                return NotFound();
+            }
+
+            return View(laptop);
+        }
+
+        // POST: Laptops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var laptop = await _context.Laptop.FindAsync(id);
+            _context.Laptop.Remove(laptop);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LaptopExists(int id)
+        {
+            return _context.Laptop.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ web3/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace web3.Models
+{
+    public class Category
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        
+        //OneToMany
+        public ICollection<Mobile> Mobiles { get; set; }
+    }
+}
