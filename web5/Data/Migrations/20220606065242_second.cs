704bbc66f144822563cc387342d68caacdb17cb1
longndt <longndt226@gmail.com>
2022-06-06 14:39:48 +0700

    web5

---
+++ web5/Data/Migrations/20220606065242_second.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace web5.Data.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Student",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Student");
+        }
+    }
+}
---
+++ web5/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web5.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [StringLength(8, ErrorMessage = "Student Id must be 8 characters")]
+        public string Sid { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string MobilePhone { get; set; }
+
+        [Range(1,4)]
+        public int StudentYear { get; set; }
+
+        [Range(0,10)]
+        public float? Grade { get; set; } //nullable ==> required = false
+ 
+        [IsBefore("01/01/2022")]
+        public DateTime BirthDay { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
