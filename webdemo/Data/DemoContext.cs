377287564c02413c1125255526acfca2cda2d2eb
longndt <longndt226@gmail.com>
2022-06-06 17:47:41 +0700

    webdemo

---
+++ webdemo/Data/DemoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webdemo.Models;
+
+//Tools => NuGet Package Manager => Package Manager Console
+// => Install-Package: Microsoft.EntityFrameworkCore (version : 5.0.10)
+
+namespace webdemo.Data
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ webdemo/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿namespace webdemo.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, E, F
+    }
+    public class Enrollment
+    {
+        //primary key (PK)
+        public int EnrollmentId { get; set; }
+        
+        //foreign key (FK)
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        
+        //data type: enum Grade
+        public Grade Grade { get; set; }
+
+        //navigation properties
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
