029bcbd93833d1d9c4e4a43eb8968c13ed161262
longndt <longndt226@gmail.com>
2022-06-15 16:54:43 +0700

    demoweb

---
+++ demoweb/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using demoweb.Data;
+using demoweb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace demoweb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = context.Category.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = context.Category
+                .Include(c => c.Products)
+                .FirstOrDefault(m => m.Id == id)
+            ;
+            return View(category);
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = context.Category.Find(id);
+            context.Category.Remove(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+           if (ModelState.IsValid) {
+                context.Category.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = context.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Category.Update(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}   
---
+++ demoweb/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace demoweb.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Product Name")]
+        [Required(ErrorMessage = "Product Name can not be empty")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 100000, ErrorMessage = "Price must be between 1$ and 100000$")]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Quantity must be between 0 and 1000")]
+        public int Quantity { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Manufacturer Date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        
+        public Category category { get; set; }
+    }   
+}
